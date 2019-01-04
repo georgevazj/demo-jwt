e0fdc888a9d91fdc1a9c2417205c5e2c91d62097
Jorge <georgevazj@gmail.com>
2019-01-04 08:14:58 +0100

    Controlador para el usuario completado

--- src/main/java/com/georgevazj/lab/demojwt/controller/UsuarioController.java
+++ src/main/java/com/georgevazj/lab/demojwt/controller/UsuarioController.java
@@ -1,7 +1,48 @@
 package com.georgevazj.lab.demojwt.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.georgevazj.lab.demojwt.model.Usuario;
+import com.georgevazj.lab.demojwt.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
+
+    private UsuarioRepository usuarioRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UsuarioController(UsuarioRepository usuarioRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Page<Usuario> getUsuarios(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    @RequestMapping(value = "/{usuario}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Usuario getUsuario(@PathVariable String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Usuario saveUsuario(@Valid @RequestBody Usuario usuario) {
+        usuario.setPassword(bCryptPasswordEncoder.encode(usuario.getPassword()));
+        return usuarioRepository.save(usuario);
+    }
 }
